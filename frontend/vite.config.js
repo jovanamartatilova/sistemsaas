4c486c3c1ec122cb7757d95ad5937e895edc6e98
ChrisantLena <chrisant.ardiyanti.lena-2023@fst.unair.ac.id>
2026-05-05 11:09:24 +0700

    benerin error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'jspdf-autotable': path.resolve(__dirname, 'node_modules/jspdf-autotable/dist/jspdf.plugin.autotable.mjs'),
+    },
+  },
+  optimizeDeps: {
+    include: ['jspdf', 'jspdf-autotable'],
+  },
 })
