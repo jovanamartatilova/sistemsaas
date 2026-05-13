277ca41c6712b5db83c12d541badcabf50211cbe
ChrisantLena <chrisant.ardiyanti.lena-2023@fst.unair.ac.id>
2026-05-13 11:52:03 +0700

    fix: add DO IP to CORS allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://167.172.83.139',
     ],
 
     'allowed_origins_patterns' => [],
